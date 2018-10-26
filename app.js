556e3aa739a402762e17f3bd378479c458edf654
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2018-10-27 03:40:14 +0530

    Create login and registration pages

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
- const express = require('express');
-const path 	= require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-const flash = require('connect-flash');
-const passport = require('passport');
-const expressSession = require('express-session');
+const flash = require("connect-flash");
+const passport = require("passport");
+const expressSession = require("express-session");
 //const expressValidator = require('express-validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const request = require("request");
 
 //mongoose.Promise = global.Promise
 //mongoose.connect("mongodb://ruraldev:ruraldev@ds125578.mlab.com:25578/rural-development");
 
-
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -30,7 +28,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
   res.render("login.ejs");
