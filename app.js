b7d7c03a4cb534a9586b47a5f4a8c9e8cd151e43
Divyansh Jamuaar <divyanshjamuaar@gmail.com>
2018-10-27 00:16:27 +0530

    kuch kuch

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-const express = require("express");
+ const express = require('express');
+const path 	= require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+const flash = require('connect-flash');
+const passport = require('passport');
+const expressSession = require('express-session');
+//const expressValidator = require('express-validator');
+const bcrypt = require('bcrypt');
+const request = require("request");
+
+//mongoose.Promise = global.Promise
+//mongoose.connect("mongodb://ruraldev:ruraldev@ds125578.mlab.com:25578/rural-development");
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//let models_path =  './models';
+//app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.render("login.ejs");
 });
 
 app.listen(3000);
